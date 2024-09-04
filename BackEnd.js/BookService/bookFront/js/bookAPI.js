545664d948bff7a13f402403a437813011ff4dbd
최세은 <seeun2190@gmail.com>
2024-09-04 17:39:52 +0900

    Feat : Book Service

--- BackEnd.js/BookService/bookFront/js/bookAPI.js
+++ BackEnd.js/BookService/bookFront/js/bookAPI.js
@@ -1,60 +1,6 @@
 const baseUrl = `http://localhost:7777`;
 const url = baseUrl + '/books';
 
-const init = () => {
-    const btnAll = document.querySelector('#btnAll');
-    const btnCreate = document.querySelector('#btnCreate');
-    const btnUpdate = document.querySelector('#btnUpdate');
-
-    btnAll.onclick = async () => {
-        getAllBooks();
-    }
-
-    btnCreate.onclick = () => { //등록 버튼
-        const title = document.getElementById('title').value;
-        const publish = document.getElementById('publish').value;
-        const price = document.getElementById('price').value;
-        if (!title || !publish || !price) {
-            alert('모든 값을 입력해야 한다')
-            return;
-        }
-        const book = { title, publish, price };
-        addBook(book);
-    }
-
-
-    //수정한 도서 정보 얻어오기
-    btnUpdate.onclick = () => {
-        const isbn = document.getElementById('isbn').value;
-        const title = document.getElementById('title').value;
-        const publish = document.getElementById('publish').value;
-        const price = document.getElementById('price').value;
-
-        //유효성 검증
-        if (!isbn || !title || !publish || !price) {
-            alert("모든 값을 입력해야 한다");
-            return;
-        }
-        if (isNaN(price)) {
-            alert('가격은 숫자여야 해요')
-            return;
-        }
-        const tmpBook = { isbn, title, publish, price };
-        updateBook(tmpBook);
-    }
-    const btn = document.querySelector('#btnSearch'); //검색 버튼
-    btnSearch.onclick = () => {
-        let keyword = document.getElementById('keyword').value;
-        if (!keyword) {
-            alert('검색할 도서명을 입력하세요');
-            document.getElementById('keyword').focus();
-            return;
-        }
-        findBook(keyword);
-    }
-    getAllBooks();
-}
-
 const findBook = async (keyword) => {
     // alert(keyword);
     const url2 = url+`/search?keyword=${keyword}`;//url 만듦
@@ -147,7 +93,6 @@ const renderBooks = (data) => {
     }
 }
 
-
 const getBook = async (isbn) => {
     console.log('getBook() isbn: ', isbn);
     try {
@@ -234,5 +179,4 @@ const updateBook = async (newBook) => {
     }
 }
 
-
-document.addEventListener('DOMContentLoaded', init)
+export {getAllBooks, addBook, getBook, findBook, deleteBook, updateBook};
